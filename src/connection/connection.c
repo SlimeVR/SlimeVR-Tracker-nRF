79e60f9ac8c62980d8c8a66d7634a3af9e276e22
sctanf <36978460+sctanf@users.noreply.github.com>
2026-02-26 14:41:18 -0600

    Send button press after timeout

--- src/connection/connection.c
+++ src/connection/connection.c
@@ -325,7 +325,7 @@ void connection_write_packet_6() // reduced precision quat and accel with button
 	k_mutex_lock(&data_buffer_mutex, K_FOREVER);
 	memcpy(data_buffer, data, sizeof(data));
 	last_data_time = k_uptime_get(); // TODO: use ticks
-	if (tracker_button && k_uptime_get() > button_update_time + 1000) // attempt to "hold" button presses for 1000 ms
+	if (tracker_button && k_uptime_get() > button_update_time + 1000) // attempt to send button press for 1000 ms
 	{
 		tracker_button = 0;
 		button_update_time = 0;
@@ -356,7 +356,7 @@ void connection_write_packet_7() // button
 	k_mutex_lock(&data_buffer_mutex, K_FOREVER);
 	memcpy(data_buffer, data, sizeof(data));
 	last_data_time = k_uptime_get(); // TODO: use ticks
-	if (tracker_button && k_uptime_get() > button_update_time + 1000) // attempt to "hold" button presses for 1000 ms
+	if (tracker_button && k_uptime_get() > button_update_time + 1000) // attempt to send button press for 1000 ms
 	{
 		tracker_button = 0;
 		button_update_time = 0;
--- src/system/system.c
+++ src/system/system.c
@@ -333,13 +333,11 @@ static void button_thread(void)
 			if (!get_status(SYS_STATUS_BUTTON_PRESSED))
 				set_status(SYS_STATUS_BUTTON_PRESSED, true);
 			num_presses++;
-			if (!CONFIG_0_SETTINGS_READ(CONFIG_0_USER_EXTRA_ACTIONS))
-				connection_update_button(num_presses);
 			LOG_INF("Button pressed %d times", num_presses);
 			last_press_duration = 0;
 			last_press = k_uptime_get();
 		}
-		if (last_press && k_uptime_get() - last_press > 1000)
+		if (last_press && k_uptime_get() - last_press > 400)
 		{
 			LOG_INF("Button was pressed %d times", num_presses);
 			last_press = 0;
@@ -347,6 +345,8 @@ static void button_thread(void)
 //				sys_request_system_reboot(false);
 			if (CONFIG_0_SETTINGS_READ(CONFIG_0_USER_EXTRA_ACTIONS)) // TODO: extra actions are default until server can send commands to trackers
 				sys_reset_mode(num_presses - 1);
+			else
+				connection_update_button(MIN(num_presses, 2)); // only single or double press
 			num_presses = 0;
 			set_status(SYS_STATUS_BUTTON_PRESSED, false);
 		}
