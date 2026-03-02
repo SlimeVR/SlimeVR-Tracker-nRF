7c14f4a79355fdd158326a4cc926a1a40fa82fbf
sctanf <36978460+sctanf@users.noreply.github.com>
2026-03-02 02:37:16 -0600

    Fix compilation

--- src/connection/connection.c
+++ src/connection/connection.c
@@ -320,9 +320,9 @@ void connection_write_packet_5() // runtime
 	data[1] = tracker_id;
 	int64_t *buf = (int64_t *)&data[2];
 	if (sys_get_valid_battery_pptt() >= 0)
-		buf[0] = k_ticks_to_us_floor64(sys_get_battery_remaining_time_estimate());
+		*buf = k_ticks_to_us_floor64(sys_get_battery_remaining_time_estimate());
 	else
-		buf[0] = -1; // no valid reading yet, but previous estimate may still be valid
+		*buf = -1; // no valid reading yet, but previous estimate may still be valid
 	k_mutex_lock(&data_buffer_mutex, K_FOREVER);
 	memcpy(data_buffer, data, sizeof(data));
 	last_data_time = k_uptime_get(); // TODO: use ticks
@@ -339,9 +339,9 @@ void connection_write_packet_6() // reduced precision quat and accel with button
 	data[2] = tracker_button;
 	uint16_t *buf = (uint16_t *)&data[3];
 	if (shutdown)
-		buf = 1;
+		*buf = 1;
 	else
-		buf = timeout_time < 1 ? 1 : timeout_time;
+		*buf = timeout_time < 1 ? 1 : timeout_time;
 	if (k_ticks_to_ms_floor64(sys_get_battery_remaining_time_estimate()) < 60000 && timeout_time == UINT16_MAX)
 		timeout_time = UINT16_MAX - 1;
 	data[15] = 0; // rssi (supplied by receiver)
@@ -366,9 +366,9 @@ void connection_write_packet_7() // button and sleep time
 	data[2] = tracker_button;
 	uint16_t *buf = (uint16_t *)&data[3];
 	if (shutdown)
-		buf = 1;
+		*buf = 1;
 	else
-		buf = timeout_time < 1 ? 1 : timeout_time;
+		*buf = timeout_time < 1 ? 1 : timeout_time;
 	if (k_ticks_to_ms_floor64(sys_get_battery_remaining_time_estimate()) < 60000 && timeout_time == UINT16_MAX)
 		timeout_time = UINT16_MAX - 1;
 	float v[3] = {0};
@@ -378,7 +378,7 @@ void connection_write_packet_7() // button and sleep time
 	uint16_t v_buf[3] = {SATURATE_UINT10((1 << 10) * v[0]), SATURATE_UINT11((1 << 11) * v[1]), SATURATE_UINT11((1 << 11) * v[2])}; // fill 32 bits
 	uint32_t *q_buf = (uint32_t *)&data[5];
 	*q_buf = v_buf[0] | (v_buf[1] << 10) | (v_buf[2] << 21);
-	uint16_t *buf = (uint16_t *)&data[9];
+	buf = (uint16_t *)&data[9];
 	buf[0] = TO_FIXED_7(sensor_a[0]);
 	buf[1] = TO_FIXED_7(sensor_a[1]);
 	buf[2] = TO_FIXED_7(sensor_a[2]);
